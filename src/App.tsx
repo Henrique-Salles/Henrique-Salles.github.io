e897b54cea7f2cfcc2933f91a1cc32dfaea52c4c
Henrique-Salles <henriquesalles1520@gmail.com>
2026-09-16 21:37:18 -0300

    Major Style Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 import Compform from './components/Compform'
 
 function App() {
-  const [weight,setWeigth]=useState<number>(0)
-  const [height,setHeigth]=useState<number>(0)
-  const [imc,setIMC]=useState<number>(0)
+  const [weight, setWeight] = useState<number | undefined>(undefined)
+  const [height, setHeight] = useState<number | undefined>(undefined)
+  const [imc, setIMC] = useState<number | undefined>(undefined)
+
 
   function calculate() {
+    if (weight === undefined || height === undefined) {
+      return
+    }
     let res=weight/(height*height)
-    setIMC(res)
+    setIMC(Number(res.toFixed(2)))
   }
 
   return (
@@ -21,22 +25,51 @@ function App() {
           <p>
             Informações do paciente:
           </p>
-          <Compform label='Peso (kg)' state={weight} funcState={setWeigth}/>
-          <Compform label='Altura (m)' state={height} funcState={setHeigth}/>
+          <Compform 
+            label='Peso (kg):' 
+            state={weight} 
+            funcState={setWeight}
+            className='weightInput'
+            placeholder='Ex: 75'/>
+          <Compform 
+            label='Altura (m):' 
+            state={height} 
+            funcState={setHeight}
+            className='heightInput'
+            placeholder='Ex: 1.70'/>
           <button className="calc-button" onClick={calculate}>Calcular</button>
           <h2>Resultado:</h2>
           <h1>{imc}</h1>
         </div>
+
+        <div id="card">
+          <div id="card__title">Tabela de Resultados</div>
+          <div id="card__data">
+            <div id="card__left">
+              <div id="item">&lt; 18,50</div>
+              <div id="item">18,50 a 24,99</div>
+              <div id="item">25 a 29,99</div>
+              <div id="item">&gt; 30</div>
+            </div>
+            <div id="card__right">
+              <div id="item">Magreza</div>
+              <div id="item">Normal</div>
+              <div id="item">Sobrepeso</div>
+              <div id="item">Obesidade</div>
+            </div>
+          </div>
+        </div>
+
       </section>
 
-      <section id="social-media">
-        <div id="docs">
+      <section id="footer">
+        <div id="references">
           <h2>Referências:</h2>
           <p>OMS: <a href="https://www.who.int/news-room/fact-sheets/detail/obesity-and-overweight" target="_blank">who.int
           </a></p>
           <p>ABESO: <a href="https://abeso.org.br/como-calcular-o-imc-entenda-para-que-serve-esse-indice/" target="_blank">abeso.org.br
           </a></p>
-        </div><div id="docs">
+        </div><div id="references">
           <h2>Desenvolvido por:</h2>
           <p>Carlos Henrique Salles Manhães</p>
           <ul>
--- src/components/Compform.tsx
+++ src/components/Compform.tsx
@@ -1,13 +1,22 @@
+import './Compform.css'
+
 interface CompformProps{
     label:string,
     state:any,
-    funcState:any
+    funcState:any,
+    className?: string,
+    placeholder?: string
 }
 export default function Compform(props: CompformProps) {
     return(
-        <div>
-            <label>{props.label}</label>
-            <input value={props.state} onChange={(evt)=> props.funcState(evt.target.value)} />
+        <div id="form">
+            <label className="labels">{props.label}</label>
+            <input 
+                className={'inputs ${props.className ?? ""}'}
+                placeholder={props.placeholder}
+                type='number' 
+                value={props.state} 
+                onChange={(evt)=> props.funcState(evt.target.value)} />
         </div>
     )
 }
